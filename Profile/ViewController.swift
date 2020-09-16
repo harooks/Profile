cb0b73a64b0722d7ee43ced240ffe522dd3ceaa6
Haruko <haruko.flipsy.krb@gmail.com>
2020-09-16 13:49:24 +0900

    added buttons

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,27 +11,59 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var profileImageView: UIImageView!
-    
+    @IBOutlet weak var profileLabel: UILabel!
     @IBOutlet weak var profileCommentLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //hide profile picture
-        profileImageView.isHidden = true
-        
-        //hide profile comment
-        profileCommentLabel.isHidden = true
+
     }
     
     @IBAction func tapButton1(){
-        //show profile picture
-        profileImageView.isHidden = false
+        //画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "philImage")
         
-        //show profile comment
-        profileCommentLabel.isHidden = false
+        //タイトルをlabelに反映する
+        profileLabel.text = "名前"
+        
+        //コメントをlabelに反映する
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
     }
 
+    @IBAction func tabButton2(){
+        //画像をImageViewに反映する
+        profileImageView.image = UIImage(named: "trackImage")
+        
+        //タイトルをlabelに反映する
+        profileLabel.text = "スポーツ"
+        
+        //コメントをlabelに反映する
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+    }
+    
+    @IBAction func tapButton3(_ sender: Any) {
+        //画像をImageViewに反映する
+         profileImageView.image = UIImage(named: "appleImage")
+         
+         //タイトルをlabelに反映する
+         profileLabel.text = "好きな食べ物"
+         
+         //コメントをlabelに反映する
+         profileCommentLabel.text = "リンゴが好きで、いつも持ち歩いている"
+    }
+    
+    @IBAction func tapButton4(_ sender: Any) {
+        //画像をImageViewに反映する
+         profileImageView.image = UIImage(named: "flightImage")
+         
+         //タイトルをlabelに反映する
+         profileLabel.text = "趣味"
+         
+         //コメントをlabelに反映する
+         profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+    }
+    
 
 }
 
